24884b3c1fb7f698179f43951fb71103d12f5ce2
Pavel Levchuk <SpaierInstinct@gmail.com>
2018-09-07 19:29:30 +0300

    build: update packages

--- package-scripts.js
+++ package-scripts.js
@@ -1,15 +1,15 @@
 const npsUtils = require('nps-utils')
 
 module.exports = {
-  compodoc: {
-    default: 'compodoc -p tsconfig.json -d docs',
-    serve: 'compodoc -s'
-  },
-  build: {
-    default: 'ng build'
-  },
-  test: {
-    default: 'ng test',
-    ci: 'ng test -c ci'
+  scripts: {
+    compodoc: {
+      default: 'compodoc -p tsconfig.json -d docs',
+      serve: 'compodoc -s'
+    },
+    build: 'ng build',
+    test: {
+      default: 'ng test',
+      ci: 'ng test -c ci'
+    }
   }
 }
