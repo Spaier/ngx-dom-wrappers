09996ec4a5686b5c222360ff943c005cb18441a8
Pavel Levchuk <SpaierInstinct@gmail.com>
2018-10-01 00:45:35 +0300

    ci: use puppeteer for e2e

--- projects/sample-e2e/protractor.conf.ci.js
+++ projects/sample-e2e/protractor.conf.ci.js
@@ -1,9 +1,11 @@
+const puppeteer = require('puppeteer')
 const config = require('./protractor.conf').config
 
 config.capabilities = {
   browserName: 'chrome',
   chromeOptions: {
-    args: ['--headless', '--no-sandbox', '--disable-gpu']
+    args: ['--headless', '--no-sandbox', '--disable-gpu'],
+    binary: process.env.HEADLESS ? puppeteer.executablePath() : undefined
   }
 }
 
