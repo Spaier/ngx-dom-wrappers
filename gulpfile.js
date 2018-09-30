0f0557f293808814b761a8ccd2c68e38fed81a9e
Pavel Levchuk <SpaierInstinct@gmail.com>
2018-10-01 00:03:57 +0300

    style: comment unused variables

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ const pump = require('pump')
 gulp.task('default', copyFiles)
 
 function copyFiles(done) {
-  let root = 'dist/ngx-dom-wrappers'
+  // let root = 'dist/ngx-dom-wrappers'
   let output = 'dist/ngx-dom-wrappers'
   pump([
     gulp.src(['README.md', 'LICENSE']),
